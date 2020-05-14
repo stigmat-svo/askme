93a3fcc94313494c06ed8dd1f3593989c9f36141
stigmat <sheludko.vo@gmail.com>
2020-05-14 18:57:48 +0300

    homework

--- app/models/user.rb
+++ app/models/user.rb
@@ -57,13 +57,13 @@ class User < ApplicationRecord
   end
 
 
-  # # Коллбэки
-  # before_validation :before_validation
-  # after_validation :after_validation
-  # before_save :before_save
-  # before_create :before_create
-  # after_create :after_create
-  # after_save :after_save
+  # Коллбэки
+  before_validation :before_validation
+  after_validation :after_validation
+  before_save :before_save
+  before_create :before_create
+  after_create :after_create
+  after_save :after_save
   #
   # before_update :before_update
   # after_update :after_update
@@ -72,14 +72,14 @@ class User < ApplicationRecord
   # after_destroy :after_destroy
   #
   # # Метод вызова коллбэков
-  # private
+  private
   #
-  # %w(validation save create update destroy).each do |action|
-  #   %w(before after).each do |time|
-  #     define_method("#{time}_#{action}") do
-  #       puts "------> #{time} #{action}"
-  #     end
-  #   end
-  # end
+  %w(validation save create update destroy).each do |action|
+     %w(before after).each do |time|
+       define_method("#{time}_#{action}") do
+         puts "------> #{time} #{action}"
+       end
+     end
+   end
 
 end
