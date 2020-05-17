035c308a0e0757795d839e89b5294b3cd0b3ca75
stigmat <sheludko.vo@gmail.com>
2020-05-17 17:14:27 +0300

    askme 1.5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
   def index
+    @users = [
+        User.new(
+            id: 1,
+            name: 'Виталий',
+            username: 'stigmat',
+            avatar_url: "https://lh3.googleusercontent.com/proxy/8RjW104aWqaFhYWwqhkf-KeAjZ8XRJDdrReNZLJkK_4PmzHWtMy_AvaNFCTb-pOOLysIH9ax6_h6BWo"
+        ),
+        User.new(
+            id: 2,
+            name: 'Игорь',
+            username: 'csg'
+        )
+    ]
   end
 
   def new
@@ -8,11 +21,24 @@ class UsersController < ApplicationController
   def edit
   end
 
-  # def show
-  #   @user = User.new(
-  #       name: "Виталий Шелудько",
-  #       username: "stigmat",
-  #       avatar_url: "https://avatars0.githubusercontent.com/u/56791300?s=460&u=e85d8ff50c15b850469d9aac4daa717355846153&v=4"
-  #   )
-  # end
+  def show
+    @user = User.new(
+        name: "Виталий Шелудько",
+        username: "stigmat",
+        avatar_url: "https://lh3.googleusercontent.com/proxy/8RjW104aWqaFhYWwqhkf-KeAjZ8XRJDdrReNZLJkK_4PmzHWtMy_AvaNFCTb-pOOLysIH9ax6_h6BWo"
+    )
+
+    @questions = [
+        Question.new(
+            text: 'Как дела?',
+            created_at: Date.parse('17.05.2020')
+        ),
+        Question.new(
+            text: 'Как семья, как работа?',
+            created_at: Date.parse('17.05.2020')
+        )
+    ]
+
+    @new_question = Question.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'users#index'
+
   resources :users
+  resources :questions
 end
