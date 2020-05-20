8d1f9a05767984a18b72c28a2e9eca8943361164
stigmat <sheludko.vo@gmail.com>
2020-05-20 15:36:43 +0300

    Adding a declination question counter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
             id: 1,
             name: 'Виталий',
             username: 'stigmat',
-            avatar_url: "https://lh3.googleusercontent.com/proxy/B0gSiyiLunjf7WMUjF1odzDYItEB6DH1yLRBMM4WQ9WwYEFPs898T4MoLGGYFQmSdUYxLv7Q9sS5Axw"
+            avatar_url: "http://zornet.ru/_fr/81/3053642.jpg"
         ),
         User.new(
             id: 2,
@@ -25,20 +25,28 @@ class UsersController < ApplicationController
     @user = User.new(
         name: "Виталий Шелудько",
         username: "stigmat",
-        avatar_url: "https://lh3.googleusercontent.com/proxy/B0gSiyiLunjf7WMUjF1odzDYItEB6DH1yLRBMM4WQ9WwYEFPs898T4MoLGGYFQmSdUYxLv7Q9sS5Axw"
+        avatar_url: "http://zornet.ru/_fr/81/3053642.jpg"
     )
 
     @questions = [
         Question.new(
             text: 'Как дела?',
-            created_at: Date.parse('17.05.2020')
+            created_at: Date.parse('17.05.2020'),
+            answer: 'Отлично!'
         ),
         Question.new(
             text: 'Как семья, как работа?',
-            created_at: Date.parse('17.05.2020')
+            created_at: Date.parse('17.05.2020'),
+            answer: 'Всё хорошо!'
+        ),
+        Question.new(
+            text: 'Машину починил?',
+            created_at: Date.parse('17.05.2020'),
+            answer: nil
         )
     ]
 
     @new_question = Question.new
+    @questions_count = @questions.count
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,19 @@ module ApplicationHelper
       asset_path 'avatar.jpg'
     end
   end
+
+  def inclination(num)
+    modul = num % 10
+    modul_2 = num % 100
+
+    if (11..14).cover?(modul_2)
+      return 'вопросов'
+    elsif modul == 1
+      return 'вопрос'
+    elsif (2..4).cover?(modul)
+      return 'вопроса'
+    else
+      return 'вопросов'
+    end
+  end
 end
