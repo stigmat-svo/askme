76aeb69fc4c8ec5ae4eb756b790269c784e84e2d
stigmat <sheludko.vo@gmail.com>
2020-05-21 00:00:40 +0300

    changed the declination method of words

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,18 +7,18 @@ module ApplicationHelper
     end
   end
 
-  def inclination(num)
+  def inclination(num, singular, singular_rod, plural_rod)
     modul = num % 10
     modul_2 = num % 100
 
     if (11..14).cover?(modul_2)
-      return 'вопросов'
+      return plural_rod
     elsif modul == 1
-      return 'вопрос'
+      return singular
     elsif (2..4).cover?(modul)
-      return 'вопроса'
+      return singular_rod
     else
-      return 'вопросов'
+      return plural_rod
     end
   end
 end
