255dc3ea5dd0e44be5997a9c4607e76c3ca271d4
stigmat <sheludko.vo@gmail.com>
2020-05-30 18:28:47 +0300

    Color and bg for profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_121947) do
+ActiveRecord::Schema.define(version: 2020_05_30_145234) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2020_05_30_121947) do
     t.string "password_salt"
     t.string "avatar_url"
     t.string "profilecolor", default: "#005a55"
+    t.string "profileimg"
+    t.string "picture"
   end
 
   add_foreign_key "questions", "users"
