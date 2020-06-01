a528409d57ff23290511a2308567d1fb8fa68c3a
stigmat <sheludko.vo@gmail.com>
2020-06-01 20:08:52 +0300

    Author of question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,7 +8,7 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new(question_params)
 
-    @question.author_id = current_user&.id if current_user.present?
+    @question.author = current_user
 
     if @question.save
       redirect_to user_path(@question.user), notice: 'Вопрос удачно создан!'
